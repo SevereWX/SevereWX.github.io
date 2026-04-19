30c4e58d2f98e48c07de829287da88a48bda55c6
SevereWX <severewx.group@gmail.com>
2026-04-20 00:48:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// kleiner "alive" Effekt (optional, sehr subtil)
+console.log("SevereWX loaded");
+
+// optional: leichte Begrüßung
+window.addEventListener("load", () => {
+    document.title = "SevereWX";
+});
