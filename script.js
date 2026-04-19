aec116d1da8007e796aafea231f3d961d71a03e9
SevereWX <severewx.group@gmail.com>
2026-04-19 20:35:40 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// kleiner "alive" Effekt (optional, sehr subtil)
-console.log("SevereWX loaded");
-
-// optional: leichte Begrüßung
-window.addEventListener("load", () => {
-    document.title = "SevereWX";
-});
-
